fad5b801d29b4add027857e468b7fb168890d446
msaleh-313 <msalehbutt313@gmail.com>
2024-07-27 05:03:11 +0500

    Seperation of Concerns

--- JQuery/Change Background Color 2/script.js
+++
@@ -1,8 +0,0 @@
-
-const colors=["red", "green", "blue", "yellow","orange","white"];
-const size=colors.length;
-$(document).ready(function() {
-    $("#btn").click(function(){
-        $("body").css("background-color",colors[Math.floor(Math.random()*size)])
-    })
-});
